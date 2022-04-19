82ee04b3d0bc4335efc252f70a4d38806bb0ead9
nicholasqnguyen8 <nicholasqnguyen8@gmail.com>
2022-04-19 19:39:29 -0400

    put the rectangle for the player in a vector

--- raylibTest1/src/main.rs
+++ raylibTest1/src/main.rs
@@ -1,41 +1,30 @@
 use raylib::prelude::*;
 use raylib::ffi::KeyboardKey::*;
+use raylib::ffi::MouseButton::*;
 use rand::Rng;
 
 // Import my rust files
-mod debug_functions;
+// mod debug_functions;
 
 // global constants
-const SCREEN_WIDTH: i32 = 640;
+const SCREEN_WIDTH: i32 = 720;
 const SCREEN_HEIGHT: i32 = 480;
 // const SLEEP_TIME: u64 = 2;
-const SPEED: f32 = 0.1;
+const SPEED: f32 = 0.05;
 
-fn main() {
 
-    // constants
-    let mut vector = Vector2::new(32.0, 32.0);
-    let mut rectangle = Rectangle::new(32.0, 32.0, 32.0, 32.0);
-
-    let (mut rl, thread) = raylib::init()
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Hello, World")
-        .build();
-
-    debug_functions::print_type_of(&rl);
-
-    while !rl.window_should_close() {
-        {
-        let mut d = rl.begin_drawing(&thread);
-        
-        d.clear_background(Color::WHITE);
-        // Draw Hello, world! and move it to a random spot
-        d.draw_text("Hello, world!", vector.x as i32, vector.y as i32, 20, Color::BLACK);
-        random_move(&mut vector);
-        // Draw a rectangle and move it with arrows
-        d.draw_rectangle_rec(rectangle, Color::BLUE);
-        }
-        update(&mut rl, &mut rectangle);
+fn update_player(rl: &mut RaylibHandle, rect: &mut Rectangle){
+    if rl.is_key_down(KEY_W){
+        rect.y = rect.y - SPEED;
+    }
+    if rl.is_key_down(KEY_S){
+        rect.y = rect.y + SPEED;
+    }
+    if rl.is_key_down(KEY_D){
+        rect.x = rect.x + SPEED;
+    }
+    if rl.is_key_down(KEY_A){
+        rect.x = rect.x - SPEED;
     }
 }
 
@@ -46,18 +35,39 @@ fn random_move(position: &mut Vector2) {
     position.y = move_y;
 }
 
-fn update(rl: &mut RaylibHandle, rect: &mut Rectangle)
-{
-    if rl.is_key_down(KEY_UP){
-        rect.y = rect.y - SPEED;
-    }
-    if rl.is_key_down(KEY_DOWN){
-        rect.y = rect.y + SPEED;
-    }
-    if rl.is_key_down(KEY_RIGHT){
-        rect.x = rect.x + SPEED;
-    }
-    if rl.is_key_down(KEY_LEFT){
-        rect.x = rect.x - SPEED;
-    }
+fn main() {
+
+    // constants
+    let mut vector = Vector2::new(32.0, 32.0);
+    let mut rectangle_vector = Vec::<Rectangle>::new();
+    let mut rectangle = Rectangle::new((SCREEN_WIDTH / 2) as f32 , (SCREEN_HEIGHT / 2) as f32, 32.0, 32.0);
+    rectangle_vector.push(rectangle);
+
+    let (mut rl, thread) = raylib::init()
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Hello, World")
+        .build();
+
+    while !rl.window_should_close() {
+        // Drawing Stuff
+        {
+            let mut d = rl.begin_drawing(&thread);
+            d.clear_background(Color::WHITE);
+            // Draw Hello, world! and move it to a random spot
+            d.draw_text("Hello, world!", vector.x as i32, vector.y as i32, 20, Color::BLACK);
+            random_move(&mut vector);
+            // Draw a rectangle and move it with arrows
+            for item in rectangle_vector.iter(){
+                d.draw_rectangle_rec(item, Color::BLUE);
+            }
+        }
+        // Updating stuff
+        {
+            update_player(&mut rl, &mut rectangle_vector[0]);
+            if rl.is_mouse_button_pressed(MOUSE_LEFT_BUTTON){
+                let mouse_position: Vector2 = rl.get_mouse_position();
+                println!("x: {}, y: {}", mouse_position.x, mouse_position.y)
+            }
+        }
+    } 
 }
